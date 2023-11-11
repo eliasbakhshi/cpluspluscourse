a121a5c5c45d08cd27122c0eb645bca2da113fdd
Elias Bakhshi <bakhshielias@gmail.com>
2023-11-11 15:21:14 +0100

    Practicing with Emuns

--- practices/sandbox.cpp
+++ practices/sandbox.cpp
@@ -19,8 +19,28 @@ void withPtr(int* num) {
 	*num += *num;
 }
 
+void setNewPrice(car &theCar, float newPrice) {
+	theCar.price = newPrice;
+}
 
 void sandbox() {
+
+	car bmw = { "X200", 33.33 };
+	cout << bmw.model << " cost you " << bmw.price << " to buy it." << endl;
+
+	bmw.price = 45.33;
+	cout << "The new cost for the " << bmw.model << " is " << bmw.price << endl;
+
+	setNewPrice(bmw, 56.44);
+	cout << "The new cost for the " << bmw.model << " is " << bmw.price << endl;
+
+	enum class Person {name, age};
+	enum class User {name, age};
+
+	Person age = Person::age;
+
+	cout << "The test value is " << static_cast<int>(age) << endl;
+
 
 	//int num = 20;
 	//withReference(num);
@@ -29,41 +49,41 @@ void sandbox() {
 	//cout << num << endl;
 
 
-	string file_content;
-	string the_line;
+	//string file_content;
+	//string the_line;
 
-	//Read from the file.
-	ifstream test_file("text.txt");
-	if (test_file.is_open()) {
-		cout << test_file.tellg() << endl;
-		while (getline(test_file, the_line)) {
-			cout << "Current position: " << test_file.tellg() << endl;
-			cout << the_line << endl;
-		}
+	////Read from the file.
+	//ifstream test_file("text.txt");
+	//if (test_file.is_open()) {
+	//	cout << test_file.tellg() << endl;
+	//	while (getline(test_file, the_line)) {
+	//		cout << "Current position: " << test_file.tellg() << endl;
+	//		cout << the_line << endl;
+	//	}
 
-		test_file.close();
-	} else cout << "Could not open the file";
+	//	test_file.close();
+	//} else cout << "Could not open the file";
 
-	//Write in the file. 
-	cout << endl << endl;
-	cout << "test2" << endl;
-	fstream test_file2("text.txt", ios::app);
-	if (test_file2.is_open()) {
-		test_file2.seekp(0, std::ios_base::beg);
-		while (getline(test_file, the_line)) {
-			cout << "ttt   " << test_file2.tellp();
-			//test_file2.seekp(0, std::ios_base::beg);
-			cout << the_line << endl;
-		}
-		//test_file2 << "This is a line.\n";
-		//test_file2 << "This is another line.\n";
-		test_file2.close();
-	} else cout << "Could not open the file";
-	cout << endl << endl;
+	////Write in the file. 
+	//cout << endl << endl;
+	//cout << "test2" << endl;
+	//fstream test_file2("text.txt", ios::app);
+	//if (test_file2.is_open()) {
+	//	test_file2.seekp(0, std::ios_base::beg);
+	//	while (getline(test_file, the_line)) {
+	//		cout << "ttt   " << test_file2.tellp();
+	//		//test_file2.seekp(0, std::ios_base::beg);
+	//		cout << the_line << endl;
+	//	}
+	//	//test_file2 << "This is a line.\n";
+	//	//test_file2 << "This is another line.\n";
+	//	test_file2.close();
+	//} else cout << "Could not open the file";
+	//cout << endl << endl;
 
 
-	cout << "End of the file. " << endl;
-	int test[3] = { 1,2,3 };
+	//cout << "End of the file. " << endl;
+	//int test[3] = { 1,2,3 };
 
 
 	/*int* valueZero = &test[1];
