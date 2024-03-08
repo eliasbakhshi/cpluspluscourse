69d518fe8e5bdb462860ff965a78a3a9c5fce9ea
Elias Bakhshi <bakhshielias@gmail.com>
2024-03-08 15:05:41 +0100

    Add GameObject tests

--- software_test/unit_testing.cpp
+++ software_test/unit_testing.cpp
@@ -4,36 +4,49 @@
 #include "../software_lib/GameObject.h"
 
 
-TEST(Unit_testing, selectGameObject) {
+TEST(GameObject_class, selectGameObject) {
 	Game game;
 
 	string object = "chair";
 	EXPECT_STREQ("Object created: chair", game.selectGameObject(object).c_str());
 
-	string object = "car";
+	object = "car";
 	EXPECT_STREQ("Object created: car", game.selectGameObject(object).c_str());
 }
 
-TEST(Unit_testing, listInteractionTypes) {
+TEST(GameObject_class, listInteractionTypes) {
 	GameObject gameObject;
 
 	EXPECT_STREQ("1) Grab\n2) Sit on\n3) Stand on\n", gameObject.listInteractionTypes().c_str());
 }
 
-TEST(Unit_testing, selectInteraction) {
+TEST(GameObject_class, startInteraction) {
 	GameObject gameObject;
 
-	//EXPECT_STREQ("1) Grab\n2) Sit on\n3) Stand on\n", gameObject.selectInteraction().c_str());
+	EXPECT_STREQ("Interaction started successfully!", gameObject.startInteraction().c_str());
 }
 
-TEST(Unit_testing, startInteraction) {
+TEST(GameObject_class, listCurrentInteractionOptions) {
 	GameObject gameObject;
 
-	//EXPECT_STREQ("1) Grab\n2) Sit on\n3) Stand on\n", gameObject.selectInteraction().c_str());
+	EXPECT_STREQ("No specific options found for this particular object!\n", gameObject.listCurrentInteractionOptions().c_str());
 }
 
-TEST(Unit_testing, abortInteraction) {
+TEST(GameObject_class, setCurrentInteractionOptions) {
 	GameObject gameObject;
 
-	//EXPECT_STREQ("1) Grab\n2) Sit on\n3) Stand on\n", gameObject.selectInteraction().c_str());
+	EXPECT_STREQ("The specific option you have chosen is: Sit on", gameObject.setCurrentInteractionOptions(1).c_str());
 }
+
+TEST(GameObject_class, startCurrentInteraction) {
+	GameObject gameObject;
+
+	EXPECT_STREQ("Current interaction started!", gameObject.startCurrentInteraction().c_str());
+}
+
+TEST(GameObject_class, abortCurrentInteraction) {
+	GameObject gameObject;
+
+	EXPECT_STREQ("Interaction aborted!", gameObject.abortCurrentInteraction().c_str());
+}
+
